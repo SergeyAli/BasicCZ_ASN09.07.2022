48906c1a1cbf394d5edea9abb0f71656d7c9b502
Sergey <aliger99@rambler.ru>
2022-07-09 20:19:05 +0700

    Задача 2

---
+++ 002/Program.cs
@@ -0,0 +1,25 @@
+﻿// С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго (блок-схема)
+
+// полное услове если есть и it и else, если только if то назаваеться не полное условие 
+
+int a,b;
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+// ==, !=, >, <, <=, >=
+if (a*a==b)
+{
+        System.Console.WriteLine("b являеться квадратом a");
+}
+else
+{
+    if(b*b==a)
+    {
+        System.Console.WriteLine("a являеться квадратом b");
+    }
+    else
+    {
+        System.Console.WriteLine("Числа не являються квадратами другдруга");
+    }
+}
