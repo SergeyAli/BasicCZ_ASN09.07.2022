6ef803d23646e4348986a8a7c7a383d16d2f0a27
Sergey <aliger99@rambler.ru>
2022-07-10 18:17:12 +0700

    Задача 6

---
+++ 006/Program.cs
@@ -0,0 +1,5 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math)
+double x,y;
+x = Math.PI; // константа чесла числа Пи
+y = Math.Sin(x);
+System.Console.WriteLine($"sin({x})={y}");
