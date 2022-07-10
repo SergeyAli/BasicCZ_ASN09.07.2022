16878eb857faad4ce558ead92c016224af8562f1
Sergey <aliger99@rambler.ru>
2022-07-10 18:40:04 +0700

    Задача 6_2

--- 006/Program.cs
+++ 006/Program.cs
@@ -14,5 +14,19 @@ double Sin(double x) //методы
     return Math.Sin(x);
 }
 
+double Sum(double a,double b) 
+{
+    return a+b;
+}
+
+
+void Pause() // метод
+{
+    System.Console.WriteLine("Press any key");
+
+}
 double y = Sin(3.14);
 System.Console.WriteLine(y);
+
+Pause();
+// System.Console.WriteLine(Pause());
