805b848f98fdddd77cf26e02979efd830b0a324f
Sergey <aliger99@rambler.ru>
2022-07-13 22:55:12 +0700

    Задача 010

---
+++ 010/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+//int a=45612;
+Console.Write("Введите число: ");
+string userA = Console.ReadLine();
+int a = Convert.ToInt32(userA);
+if (a<10)
+{
+    System.Console.WriteLine(a);
+}
+else
+{
+    int c = a%10;
+    System.Console.WriteLine(c);  
+}
+    
