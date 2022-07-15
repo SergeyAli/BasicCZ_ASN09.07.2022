5f5d8917460ce3d6b5e09f7d53af065baf1730a5
«Sergey» <aliger99@rambler.ru>
2022-07-15 23:38:59 +0700

    Задача 024

--- 018/Program.cs
+++ 018/Program.cs
@@ -1,27 +1,33 @@
-﻿// По двум заданным числам проверять является ли одно квадратом другого. Используйте подпрограмму.
-bool Check(int a,int b)
+﻿internal class Program
 {
-    return a*a==b;
-}
+    private static void Main(string[] args)
+    {
+        // По двум заданным числам проверять является ли одно квадратом другого. Используйте подпрограмму.
+        bool Check(int a, int b)
+        {
+            return a * a == b;
+        }
 
-int a,b;
-string? s=Console.ReadLine();
-a=Convert.ToInt32(s);
-s=Console.ReadLine();
-b=Convert.ToInt32(s);
-//==,!=,>,<,>=,<=
-if (Check(a,b))
-{
-    System.Console.WriteLine("b является квадратом a");
-}
-else
-{
-    if (Check(b,a))
-    {
-        System.Console.WriteLine("a является квадратом b");
-    }
-    else
-    {
-        System.Console.WriteLine("Числа не являются квадратами друг друга");
+        int a, b;
+        string? s = Console.ReadLine();
+        a = Convert.ToInt32(s);
+        s = Console.ReadLine();
+        b = Convert.ToInt32(s);
+        //==,!=,>,<,>=,<=
+        if (Check(a, b))
+        {
+            Console.WriteLine("b является квадратом a");
+        }
+        else
+        {
+            if (Check(b, a))
+            {
+                Console.WriteLine("a является квадратом b");
+            }
+            else
+            {
+                Console.WriteLine("Числа не являются квадратами друг друга");
+            }
+        }
     }
 }
---
+++ 024/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+Console.Write("Введите число до которого нужно выводить таблицу квадратов: ");
+string userN = Console.ReadLine();
+int n = Convert.ToInt32(userN);
+double c;
+for(int i = 1; i<=n; i++)
+{
+    c= (Math.Pow((i), 2));  
+    System.Console.WriteLine($"| {i} => {c} | ");
+}
