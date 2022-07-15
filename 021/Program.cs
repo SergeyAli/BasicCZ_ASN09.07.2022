2fa04b0814cd9bd65909a048d6605f319caf194a
«Sergey» <aliger99@rambler.ru>
2022-07-15 13:50:00 +0700

    Задача 021

---
+++ 021/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("Введите номер четверти: ");
+string userA = Console.ReadLine();
+int a = Convert.ToInt32(userA);
+string[] coordinateRange = new string[] {"Введенные данные не соответствует условию" , "Диапазон первой четверти +X +Y", "Диапазон второй четверти -X +Y", "Диапазон третьей четверти -X -Y", "Диапазон четвертой четверти +X -Y"};
+
+if ((a==1)^(a==2)^(a==3)^(a==4))
+{
+    System.Console.WriteLine(coordinateRange[a]);
+}
+else
+{
+    System.Console.WriteLine(coordinateRange[0]);
+}
+
