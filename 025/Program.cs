67d38e383e64b9255d7d955831791442fb414145
«Sergey» <aliger99@rambler.ru>
2022-07-15 23:45:04 +0700

    Задача 025

---
+++ 025/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+Console.Write("Введите число до которого нужно выводить таблицу кубов: ");
+string userN = Console.ReadLine();
+int n = Convert.ToInt32(userN);
+double c;
+for(int i = 1; i<=n; i++)
+{
+    c= (Math.Pow((i), 3));  
+    System.Console.WriteLine($"| {i} => {c} | ");
+}
