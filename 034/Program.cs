7ff1b689fe061339615c00ebdcce2023e6ec15ab
Sergey <aliger99@rambler.ru>
2022-07-17 18:28:58 +0700

    Задача 034_Масив

--- 034/Program.cs
+++ 034/Program.cs
@@ -2,6 +2,8 @@
 // [-9, 9]. Найти сумму положительных/отрицательных 
 //элементов массива
 
+//Простой вариант
+/*
 int N=12;
 int[] a=new int[N];
 Random random=new Random();//псевдослучайные числа
@@ -24,3 +26,42 @@ for(int i=0;i<a.Length;i++)
         sumNegative+=a[i];
         
 System.Console.WriteLine(sumNegative);
+*/
+// Вариант с методом
+//инициализация массива
+int[] a;
+Init(out a,12,-9,9);
+//вывод массива (до обработки)
+Print(a);
+//решение задачи
+int sumPositive;  
+int sumNegative;
+Solve(a,out sumPositive, out sumNegative);
+//Вывод результатов
+System.Console.WriteLine(sumPositive);          
+System.Console.WriteLine(sumNegative);
+void Init(out int[] t, int Length,int min=0,int max=10)
+{
+  t=new int[Length];
+  Random random=new Random();
+  for(int i=0;i<t.Length;i++) 
+    t[i]=random.Next(min,max+1);
+}
+
+void Print(int[] t)
+{
+  for(int i=0;i<t.Length;i++)
+  System.Console.Write($"{t[i],4} ");
+}
+
+void Solve(int[] a,out int sumPositive, out int sumNegative)
+{
+    sumPositive=0;
+    sumNegative=0;
+    for(int i=0;i<a.Length;i++)
+        if (a[i]>0)
+            sumPositive=sumPositive+a[i];
+    for(int i=0;i<a.Length;i++)
+        if (a[i]<0)
+            sumNegative+=a[i];
+}
