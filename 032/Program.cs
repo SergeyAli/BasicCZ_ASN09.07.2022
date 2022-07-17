199e43088915d10bf611039a13097c87e0690b97
Sergey <aliger99@rambler.ru>
2022-07-17 18:22:25 +0700

    Задача 032_2

--- 032/Program.cs
+++ 032/Program.cs
@@ -9,15 +9,40 @@ t5 = 17;
 t6 = 18;
 t7 = 17;
 double sr=(t1+t2+t3+t4+t5+t6+t7)/7;
-*/
+
 
 
 int[] t; // обявление
 t = new int[8];
 t[0]=10;
 t[1]=12;
+// Иницилизация
 for(int i=0;i<t.Length;i++)
     t[i]=i*10;
+// решение задач
+
+// ввывод результатов
 for(int i=0;i<t.Length;i++)
     System.Console.Write($"a[{i}]={t[i]} ");
-    
+*/
+ int[] t;//объявление
+//инициализация
+Init(out t,8,max:100);
+//решение задачи
+//вывод результатов на экран
+Print(t,"t");
+
+
+void Init(out int[] t, int Length,int min=0,int max=10)
+{
+  t=new int[Length];
+  Random random=new Random();
+  for(int i=0;i<t.Length;i++) 
+    t[i]=random.Next(min,max+1);
+}
+
+void Print(int[] t, string variableName)
+{
+  for(int i=0;i<t.Length;i++)
+  System.Console.Write($"{variableName}[{i}]={t[i]} ");
+}
