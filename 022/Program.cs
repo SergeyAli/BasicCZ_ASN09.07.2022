b5216b86d83def37e34ba8608443c1aa13f2872e
«Sergey» <aliger99@rambler.ru>
2022-07-21 09:09:15 +0700

    Задача_022_2

--- 022/Program.cs
+++ 022/Program.cs
@@ -1,10 +1,6 @@
 ﻿// Программа проверяет пятизначное число на палиндромом
 
-//int N=5;
 int[] a={1, 2, 3, 2, 1};
-//Random random=new Random();//псевдослучайные числа
-//for(int i=0;i<a.Length;i++)
-   // a[i]=random.Next(1,11);
 System.Console.Write("Число ");
 //вывод массива
 for(int i=0;i<a.Length;i++)
@@ -12,8 +8,7 @@ for(int i=0;i<a.Length;i++)
 //решение задачи
 int[] b=new int[a.Length/2];
 for(int i=0;i<a.Length/2;i++)
-     //b[i]=a[i]*a[a.Length-i-1];
-     
+   
 //вывод массива 
 System.Console.WriteLine();
 
@@ -25,5 +20,3 @@ if (a[0]==a[a.Length-1] & a[1]==a[a.Length-2])
     {
         System.Console.WriteLine("Не палиндромом");
     }
-//for(int i=0;i<b.Length;i++)
-   // System.Console.Write($"{b[i],4}");
