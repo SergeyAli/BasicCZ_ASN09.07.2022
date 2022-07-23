40080e90f3762d5071c2d3b2780f3cdf896013f3
«Sergey» <aliger99@rambler.ru>
2022-07-23 16:44:17 +0700

    Задача 046

---
+++ 046/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать числа Фибоначчи
+
+
+
+Console.Write("Введите конец диапазона чисел Фибоначчи от 1 до : ");
+int end=int.Parse(Console.ReadLine());
+
+ComputeF(end, 1);
+
+  void ComputeF(int end, int j)
+{
+  for(int i=1; i<=end; i+=j)
+  {
+   Console.Write("{0} ", i);
+   j=i-j;
+  }
+
+}
+
+
+
