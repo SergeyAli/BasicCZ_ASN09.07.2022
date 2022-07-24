a1941f9d41e8aa62590c01d6b4c42defe2d153fb
Sergey <aliger99@rambler.ru>
2022-07-24 16:31:23 +0700

    Задача 026

---
+++ 026/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите конец арефметической прогресии: ");
+int end=int.Parse(Console.ReadLine());
+int sum;
+SumA(end);
+
+Print(sum,end);
+
+void SumA(int end)
+{
+    sum = (end*(end+1)/2);
+}
+
+void Print(int sum,int end)
+{
+  System.Console.Write($"Арефметическая прогресия от 1 до {end} будет - {sum}");
+}
