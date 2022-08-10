8b233946a1406041ceceb962794a28b7da965e9f
Sergey <aliger99@rambler.ru>
2022-08-10 11:55:29 +0700

    Задача 004_1_2

--- 20220806/004_1/Program.cs
+++ 20220806/004_1/Program.cs
@@ -14,3 +14,29 @@ for (int i = 0; i < N; i++)
           a[i,j]=int.Parse(ss[j]);
 }
 
+Print(a);
+int max=0;
+int imax=0;
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    int sum=0;
+    for (int j = 0; j < a.GetLength(1); j++)
+        sum=sum+a[i,j];
+    if (sum>max)
+    {
+    max=sum;
+    imax=i;
+    }
+}
+
+Console.WriteLine(imax+1);
+
+//Вывод массива на экран
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j],4} ");
+            System.Console.WriteLine();
+    }
+}
