4d6dc51ec24a79d1639a8626b44796712fe323e7
Sergey <aliger99@rambler.ru>
2022-08-10 10:33:17 +0700

    Задача 040_1_1

--- 20220806/004_1/Program.cs
+++ 20220806/004_1/Program.cs
@@ -1,8 +1,16 @@
 ﻿// максимальная строка
 // В матрице найти номер строки, сумма чисел в которых максимально
-/*
-string s=Console,ReadLine();
-int  N=int.Parse(s.Split(' ')[0]);
-int  M=int.Parse(s.Split(' ')[1]);
+using System;
+
+string s=Console.ReadLine();
+int N=int.Parse(s.Split(' ')[0]);
+int M=int.Parse(s.Split(' ')[1]);
 int[,] a=new int[N,M];
-*/
+for (int i = 0; i < N; i++)
+{
+    s=Console.ReadLine();
+    string[] ss=s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+    for (int j = 0; j < ss.Length; j++)
+          a[i,j]=int.Parse(ss[j]);
+}
+
