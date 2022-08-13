db0d96536b63fc995bac8d896e8f76ad6383f47a
Sergey <aliger99@rambler.ru>
2022-08-13 20:02:34 +0700

    Задача 070

---
+++ 070/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму цифр числа - 2
+// 70. Найти сумму цифр числа - 2
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма цифр числа:"+Sum(num));
+static int Sum(int num)
+{
+    if (num < 10) return num;
+    else return num % 10 + Sum(num / 10);
+}
+
---
+++ 072A/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу возведения числа А в целую степень B - 3
+
+int P(int a, int b)
+{
+    if (b==0) return 1;
+    if (b%2!=0) return P(a,b-1)*a; 
+    else  return P(a,b/2)*P(a,b/2);
+}
+
+System.Console.WriteLine(P(2,10));
