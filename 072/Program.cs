7e1d8ba1ce4e34b7f41f7600f42394490a192f54
Sergey <aliger99@rambler.ru>
2022-08-13 20:22:34 +0700

    Задача 072

---
+++ 072/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую степень B - 3
+
+int a=2;
+int b=3;
+
+int P(int a, int b)
+{
+    if (b==0) return 1;
+    else  return P(a,b-1)*a;
+}
+
+System.Console.WriteLine(P(a,b));
+
