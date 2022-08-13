b4d02352cbe94f837587990c1890f629af9f5c7e
Sergey <aliger99@rambler.ru>
2022-08-13 20:13:58 +0700

    Задача 069

---
+++ 069/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму элементов от M до N, N и M заданы - 1
+// M = 1 N = 10
+int P(int M, int N)
+{
+    if (M==N) return M;
+    else return P(M+1,N) + M;
+}
+System.Console.WriteLine(P(1,10));
