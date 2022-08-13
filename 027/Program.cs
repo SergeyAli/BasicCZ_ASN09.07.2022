a9fcb310127d4a28241e2ff99dce0cddc5495df7
Sergey <aliger99@rambler.ru>
2022-08-13 18:54:51 +0700

    Recursy

---
+++ 027/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+/*
+Console.Write("Введите число A которое будет : ");
+int a,b;
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+
+
+
+
+
+ Console.WriteLine($"Сумма числа {b} будет {sum}");
+ */
---
+++ 20220806/Recursy/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести 
+
+void Loop(int i, int N)
+{
+    System.Console.WriteLine(i);
+    if (i<N) Loop(i+1,N);
+}
+
+Loop(1,10);
