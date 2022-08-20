6fa75abbc9e3b6bebb8855c93cdfd329c062fb03
Sergey <aliger99@rambler.ru>
2022-08-20 18:44:36 +0700

    Задача 054_1

--- 054/Program.cs
+++ 054/Program.cs
@@ -8,3 +8,45 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] array = {{1, 4, 7, 2},
+             {5, 9, 2, 3},
+             {8, 4, 2, 4}
+             };
+
+WriteArray(array);
+
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+WriteArray(array);
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
