c17c8ed64d41ec4892a56e304f744ba8c1d31004
Sergey <aliger99@rambler.ru>
2022-08-20 21:19:08 +0700

    Задача 066

---
+++ 066/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+System.Console.WriteLine("Введите M");
+int M=int.Parse(Console.ReadLine());
+// string s=Console.ReadLine();
+System.Console.WriteLine("Введите N");
+int N=int.Parse(Console.ReadLine());
+
+int P(int M, int N)
+{
+    if (M==N) return M;
+    else return P(M+1,N) + M;
+}
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N равна {P(M,N)}");
+
